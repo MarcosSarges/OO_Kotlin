19805fb85bd6d28460337c8afb92056d64ac76c5
Marcos Sarges <dev.marcos.sarges@gmail.com>
2021-03-28 21:30:34 -0300

    construtor e funcoes

--- src/main/kotlin/mp/dio/diobank/Pessoa.kt
+++ src/main/kotlin/mp/dio/diobank/Pessoa.kt
@@ -1,16 +1,23 @@
 package mp.dio.diobank
 
 class Pessoa {
+
     var nome: String = "Marcos"
 
     var cpf: String = "123.456.789-99"
         private set
+
+    constructor() {
+
+    }
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 
 fun main() {
     val mp = Pessoa()
 
-    println(mp.cpf + " " + mp.nome)
+    println(mp.pessoaInfo())
 
 }
